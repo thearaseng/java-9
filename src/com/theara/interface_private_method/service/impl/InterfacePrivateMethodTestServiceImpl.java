2ae2c97ecf512213bddba2add8d57891492a378e
Theara Seng <seng.theara.kh@gmail.com>
2018-02-07 15:15:12 +0700

    rename TestServiceImpl to InterfacePrivateMethodTestServiceImpl

--- src/com/theara/interface_private_method/service/impl/InterfacePrivateMethodTestServiceImpl.java
+++ src/com/theara/interface_private_method/service/impl/InterfacePrivateMethodTestServiceImpl.java
@@ -3,7 +3,7 @@ package com.theara.interface_private_method.service.impl;
 import com.theara.interface_private_method.IInterfaceImpl;
 import com.theara.service.TestService;
 
-public class TestServiceImpl implements TestService {
+public class InterfacePrivateMethodTestServiceImpl implements TestService {
 
     @Override
     public void test() {
