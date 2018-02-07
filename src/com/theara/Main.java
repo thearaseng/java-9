c1c9b915b55444f1b2d52371c6546c80b8412e65
Theara Seng <seng.theara.kh@gmail.com>
2018-02-07 13:29:11 +0700

    add doTest() to Main.java

--- src/com/theara/Main.java
+++ src/com/theara/Main.java
@@ -1,8 +1,17 @@
 package com.theara;
 
+import com.theara.service.TestService;
+
 public class Main {
 
     public static void main(String[] args) {
 
+
+
     }
+
+    private void doTest(TestService testService) {
+        testService.test();
+    }
+
 }
